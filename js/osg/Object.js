d58500e010f91357f962b133b453fb7b0f56e486
Cedric Pinson <cedric.pinson@plopbyte.com>
2013-04-05 18:24:31 -0400

    Add id on object, cache the trace of the cull traversal

--- js/osg/Object.js
+++ js/osg/Object.js
@@ -5,45 +5,49 @@
  *  @class Object
  */
 osg.Object = function() {
-	this._name = undefined;
-	this._userdata = undefined;
+    this._name = undefined;
+    this._userdata = undefined;
     this._dirty = true;
+    this._objectID = osg.Object.objectInstance++;
 };
-
+osg.Object.objectInstance = 0;
 /** @lends osg.Object.prototype */
 osg.Object.prototype = osg.objectLibraryClass({
-	setName: function(name) {
-		this._name = name;
-	},
-	getName: function() {
-		return this._name;
-	},
-	dirty: function() {
-		this._dirty = true;
-	},
-	setClean: function() {
-		this._dirty = false;
-	},
-	isDirty: function() {
-		return this._dirty;
-	},
-	setUserData: function(data) {
-		this._userdata = data;
-	},
-	getUserData: function() {
-		return this._userdata;
-	},
-	toString: function() {
-		return JSON.stringify(this.toObject());
-	},
-	toJSON: function() {
-		return JSON.stringify(this.toObject());
-	},
-	toObject: function() {
-		var object = {
-			_name: this._name,
-			_userdata: this._userdata
-		};
-		return object;
-	}
+    getObjectID: function() { 
+        return this._objectID; 
+    },
+    setName: function(name) {
+	this._name = name;
+    },
+    getName: function() {
+	return this._name;
+    },
+    dirty: function() {
+	this._dirty = true;
+    },
+    setClean: function() {
+	this._dirty = false;
+    },
+    isDirty: function() {
+	return this._dirty;
+    },
+    setUserData: function(data) {
+	this._userdata = data;
+    },
+    getUserData: function() {
+	return this._userdata;
+    },
+    toString: function() {
+	return JSON.stringify(this.toObject());
+    },
+    toJSON: function() {
+	return JSON.stringify(this.toObject());
+    },
+    toObject: function() {
+	var object = {
+	    _name: this._name,
+	    _userdata: this._userdata
+	};
+	return object;
+    }
 }, "osg", "Object");
