4ec3805a8e83c6d56405a82aa4eebfb978c39d41
Cedric Pinson <cedric@plopbyte.com>
2014-11-13 03:26:51 +0100

    Limit max mipmap, 2x2 pixels

--- examples/pbr/shaders/cubemapSampler.glsl
+++ examples/pbr/shaders/cubemapSampler.glsl
@@ -23,37 +23,34 @@ vec4 textureCubeLodEXTFixed(const in samplerCube texture, const in vec3 directio
     // (512 - (2.0*MaxLod - lod )) / 512;
 
     vec3 dir = direction;
-    float lod = min( 8.0, lodInput );
+    float lod = min( 7.0, lodInput );
 #ifdef FLOAT_CUBEMAP_SEAMLESS
 
     float lod0 = floor( lod );
     float lod1 = ceil( lod );
 
-    float invSize = 2.0/uEnvironmentSize.x;
-    float scale0 = 1.0 - exp2(lod0) * invSize;
-    float scale1 = 1.0 - exp2(lod1) * invSize;
-    scale0 = max(0.5, scale0 );
-    scale1 = max(0.5, scale1 );
+    vec2 lodx = vec2(lod0, lod1);
+    float invSize = 2.0/exp2(uEnvironmentMaxLod);
 
-#if 0
-    float cm0 = exp2(uEnvironmentMaxLod-lod0);
-    scale0 = max(1.0, (cm0 - 2.0) ) / cm0;
+    // compute a scale factor for seamless cubemap
+    // the max 0.5 it's because we dont want to
+    // to have a scale of 0 for lower mipmap
+    // must stop before 2x2 pixel mipamp or
+    // need to max(0.5, scale) to avoid scale of 0
+    vec2 scale = vec2(1.0) - exp2(lodx) * invSize;
 
-    float cm1 = exp2(uEnvironmentMaxLod-lod1);
-    scale1 = max(1.0, (cm1 - 2.0) ) / cm1;
-#endif
 
     float M = max(max(abs(dir.x), abs(dir.y)), abs(dir.z));
 
     vec3 dir0 = dir;
     vec3 dir1 = dir;
-    if (abs(dir.x) != M) dir0.x *= scale0;
-    if (abs(dir.y) != M) dir0.y *= scale0;
-    if (abs(dir.z) != M) dir0.z *= scale0;
+    if (abs(dir.x) != M) dir0.x *= scale[0];
+    if (abs(dir.y) != M) dir0.y *= scale[0];
+    if (abs(dir.z) != M) dir0.z *= scale[0];
 
-    if (abs(dir.x) != M) dir1.x *= scale1;
-    if (abs(dir.y) != M) dir1.y *= scale1;
-    if (abs(dir.z) != M) dir1.z *= scale1;
+    if (abs(dir.x) != M) dir1.x *= scale[1];
+    if (abs(dir.y) != M) dir1.y *= scale[1];
+    if (abs(dir.z) != M) dir1.z *= scale[1];
 
 
     vec3 p0 = textureCubeLodEXT( texture, dir0, lod0 ).rgb;
